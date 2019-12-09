3381317b2d6eaaa7971cac03235abcc3dc4a2a93
Gabriel Rufino <contato@gabrielrufino.com>
2019-12-08 22:31:43 -0300

    Log do chat.id

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,6 +1,6 @@
 const ping = context => {
-    console.log(context.update.message.chat.id)
-    context.reply('Estou vivo!')
+  console.log(context.update.message.chat.id)
+  context.reply('Estou vivo!')
 }
 
 module.exports = ping
