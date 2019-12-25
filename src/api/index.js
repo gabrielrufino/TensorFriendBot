f1b5e8ac9856feeb5b725d200836f99539a264f3
Gabriel Rufino <contato@gabrielrufino.com>
2019-12-25 11:37:08 -0300

    Correções do standard

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 const api = () => {
   const express = require('express')
-  
+
   const app = express()
-  
+
   app.get('/', (_, response) => {
     response.json({ alive: true })
   })
-  
+
   app.listen(process.env.PORT || 8080)
 }
 
