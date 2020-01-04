11a9047fa6e27ec0cdad2e29d6529c5b36759ccc
Gabriel Rufino <contato@gabrielrufino.com>
2020-01-04 09:45:40 -0300

    Ouvindo evento da saída de um membro e pedindo feedback de melhoria ao grupo

--- src/events/index.js
+++ src/events/index.js
@@ -1,7 +1,9 @@
+const leftChatMember = require('./left_chat_member')
 const newChatMembers = require('./new_chat_members')
 const photo = require('./photo')
 
 const events = bot => {
+  bot.on('left_chat_member', leftChatMember)
   bot.on('new_chat_members', newChatMembers)
   bot.on('photo', photo(bot))
 }
---
+++ src/events/left_chat_member.js
@@ -0,0 +1,7 @@
+const leftChatMember = context => {
+  context.reply(
+    'Hey! Percebi que alguém saiu do grupo 😢 O que vocês sugerem para que possamos melhorar nossa comunidade? Alguma ideia pra manter todo mundo junto aprendendo e compartilhando conhecimento sobre Machine Learning, TensorFlow e JavaScript? 🤗'
+  )
+}
+
+module.exports = leftChatMember
