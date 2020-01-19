e422293113cbfd3260b30edbe42b00f4e93f216a
Gabriel Rufino <contato@gabrielrufino.com>
2020-01-19 12:18:26 -0300

    Mensagens de boas vindas aleatórias

--- src/events/new_chat_members.js
+++ src/events/new_chat_members.js
@@ -1,7 +1,30 @@
 const newChatMembers = context => {
-  context.reply(
-    `Olá, ${context.from.first_name}. Bem vindo ao TensorFlow.js Brasil! :D`
-  )
+  const { first_name: firstName } = context.from
+
+  const responses = [
+    [
+      `Olá, ${firstName}. Seja bem vindo(a) ao TensorFlow.js Brasil! 😄`,
+      'Conta um pouco sobre quem é você!'
+    ],
+    [
+      `Seja muito bem vindo ao TensorFlow.js Brasil, ${firstName}.`,
+      'A proposta dessa comunidade é o compartilhamento de conhecimentos em torno de TensorFlow, Machine Leaning, Javascript e afins 🧐'
+    ],
+    [
+      `Hey, ${firstName}. Obrigado por juntar-se à nossa comunidade. 😊`,
+      'Tenho certeza que você tem muito a contribuir e aprender conosco!'
+    ],
+    [
+      '+1 Membro 😍 Uhuuul!',
+      `Seja muito bem vindo(a) ao TensorFlow.js Brasil, ${firstName}.`
+    ]
+  ]
+
+  const response = responses[Math.floor(Math.random() * responses.length)]
+
+  response.forEach(async text => {
+    await context.reply(text)
+  })
 }
 
 module.exports = newChatMembers
