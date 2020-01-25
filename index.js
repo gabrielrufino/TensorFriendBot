5c8c20660fc59bcf74e9831f6026b6c8615870a8
Gabriel Rufino <contato@gabrielrufino.com>
2020-01-25 17:14:24 -0300

    src => app; Movendo códigos em index.js para app/index.js

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-require('dotenv').config()
-
+const app = require('./app')
 const bootstrap = require('./bootstrap')
 
 bootstrap()
-  .then(() => console.log('Deu certo'))
-const Telegraf = require('telegraf')
-const api = require('./src/api')
-const commands = require('./src/commands')
-const events = require('./src/events')
-const helpers = require('./src/helpers')
-const routines = require('./src/routines')
-
-const bot = new Telegraf(process.env.BOT_TOKEN)
-
-api()
-commands(bot)
-routines(bot, helpers)
-events(bot)
-
-bot.catch(async error => {
-  console.error('Erro no telegraf\n', error)
-})
-
-bot.launch()
-  .then(() => {
-    console.log('TensorFriend funcionando!')
-  })
-  .catch(error => {
-    console.error(error)
-  })
+  .then(app)
