42e02eeeab066a9bf595546a30734de4336375d2
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-07 23:05:35 -0300

    Using text classification for text event

--- app/events/index.js
+++ app/events/index.js
@@ -1,11 +1,13 @@
 const leftChatMember = require('./left_chat_member')
 const newChatMembers = require('./new_chat_members')
 const photo = require('./photo')
+const text = require('./text')
 
 const events = bot => {
   bot.on('left_chat_member', leftChatMember)
   bot.on('new_chat_members', newChatMembers)
   bot.on('photo', photo(bot))
+  bot.on('text', text)
 }
 
 module.exports = events
