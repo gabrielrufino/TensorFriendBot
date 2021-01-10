098a64ceab9714ac6507497be9e1bd5c429e9a9c
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-10 10:41:09 -0300

    0.0001 => 0.15

--- app/events/text.js
+++ app/events/text.js
@@ -10,7 +10,7 @@ const text = context => {
   const firstClassification = classifications[0]
   const lastClassification = classifications[classifications.length - 1]
 
-  if ((firstClassification.value - lastClassification.value) > 0.0001) {
+  if ((firstClassification.value - lastClassification.value) > 0.15) {
     const intention = intentions.find(intention => intention.code === firstClassification.label)
     const { answers } = intention
 
