76ec63755a6571cae959ce4ad6aa196c5ded74e8
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-10 10:52:18 -0300

    Validate if param is an array

--- app/helpers/get-random-position.js
+++ app/helpers/get-random-position.js
@@ -1,3 +1,7 @@
-const getRandomPosition = array => Math.floor(Math.random() * array.length)
+const getRandomPosition = array => {
+  if (!Array.isArray(array)) throw new Error('Argument should be an array')
+
+  return Math.floor(Math.random() * array.length)
+}
 
 module.exports = getRandomPosition
