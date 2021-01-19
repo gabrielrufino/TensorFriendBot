d00cf06583ed2e7f05053e56d9a5bdb429f2fa88
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-18 22:50:54 -0300

    Using joinName in generateQuizPodium

--- app/helpers/generate-quiz-podium.js
+++ app/helpers/generate-quiz-podium.js
@@ -1,12 +1,14 @@
+const joinName = require('./join-name')
+
 const empty = '[Aguardando você]'
 
 const generateQuizPodium = (podium = {}) => {
   return `
 Pódio:
 
-🥇 1º - ${(podium.first && podium.first.first_name) || empty}
-🥈 2º - ${(podium.second && podium.second.first_name) || empty}
-🥉 3º - ${(podium.third && podium.third.third_name) || empty}
+🥇 1º - ${(podium.first && joinName(podium.first)) || empty}
+🥈 2º - ${(podium.second && joinName(podium.second)) || empty}
+🥉 3º - ${(podium.third && joinName(podium.third)) || empty}
 `
 }
 
