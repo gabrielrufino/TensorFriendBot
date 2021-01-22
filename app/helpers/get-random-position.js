bec993f707a7abb330a33d89fcb9fa46c19ec907
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-22 16:11:21 -0300

    JSDoc for get-random-position return

--- app/helpers/get-random-position.js
+++ app/helpers/get-random-position.js
@@ -3,6 +3,7 @@
 /**
  * Generate a random position of an array
  * @param {*[]} array - Array of anything
+ * @returns {number}
  */
 const getRandomPosition = array => {
   if (!Array.isArray(array)) throw new Error('Argument should be an array')
