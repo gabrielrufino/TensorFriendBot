daa397f89b81a9bd3bc8054ddfc2e18de45fc685
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-22 07:13:38 -0300

    jsdoc for join-name.js

--- app/helpers/join-name.js
+++ app/helpers/join-name.js
@@ -1,5 +1,11 @@
 'use strict'
 
+/**
+ * Returns a joined name from a telegram user
+ * @param {Object} user - Telegram user
+ * @param {string} user.first_name - First name
+ * @param {string} user.last_name - Last name
+ */
 const joinName = user => `${user.first_name} ${user.last_name}`
 
 module.exports = joinName
