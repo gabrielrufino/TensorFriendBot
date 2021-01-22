7ce477474a4c03324efe1e6ec06d52e8f3d69d0b
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-22 16:14:01 -0300

    JSDoc for romove-repetitions return

--- app/helpers/remove-repetitions.js
+++ app/helpers/remove-repetitions.js
@@ -3,6 +3,7 @@
 /**
  * Returns an array without repetitions from the given array
  * @param {*[]} array - Array of anything
+ * @returns {*[]}
  */
 const removeRepetitions = array => [...new Set(array)]
 
