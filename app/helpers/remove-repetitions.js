f561b3feeee58881a5ac700d099fa552ba527378
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-22 07:15:16 -0300

    JSDoc for remove-repetitions.js

--- app/helpers/remove-repetitions.js
+++ app/helpers/remove-repetitions.js
@@ -1,5 +1,9 @@
 'use strict'
 
+/**
+ * Returns an array without repetitions from the given array
+ * @param {*[]} array - Array of anything
+ */
 const removeRepetitions = array => [...new Set(array)]
 
 module.exports = removeRepetitions
