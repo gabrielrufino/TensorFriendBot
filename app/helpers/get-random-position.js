8296d3aa1c207677a2bda28fef5eb8766002ec05
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-22 07:07:58 -0300

    jsdoc for get-random-position

--- app/helpers/get-random-position.js
+++ app/helpers/get-random-position.js
@@ -1,5 +1,9 @@
 'use strict'
 
+/**
+ * Generate a random position of an array
+ * @param {*[]} array - Array of anything
+ */
 const getRandomPosition = array => {
   if (!Array.isArray(array)) throw new Error('Argument should be an array')
 
