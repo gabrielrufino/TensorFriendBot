cff5e5f76d027eb50be24f3081aa104a1164fa00
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-22 16:12:27 -0300

    JSDoc for join-name return

--- app/helpers/join-name.js
+++ app/helpers/join-name.js
@@ -5,6 +5,7 @@
  * @param {Object} user - Telegram user
  * @param {string} user.first_name - First name
  * @param {string} user.last_name - Last name
+ * @returns {string}
  */
 const joinName = user => `${user.first_name} ${user.last_name}`
 
