67ca2e5dc9c89d50d28fc652b4c5420888a770e9
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-23 09:23:17 -0300

    Fixing bug: undefined being shown as last name

--- app/helpers/join-name.js
+++ app/helpers/join-name.js
@@ -7,6 +7,6 @@
  * @param {string} user.last_name - Last name
  * @returns {string}
  */
-const joinName = user => `${user.first_name} ${user.last_name}`
+const joinName = user => `${user.first_name} ${user.last_name || ''}`
 
 module.exports = joinName
